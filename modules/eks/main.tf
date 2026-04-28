eed9adb0a76698899a64e973e3f7285678b9fb7f
praveen bhat <pbhat@praveens-MacBook-Air.fritz.box>
2026-04-28 23:02:36 +0200

    corrected modules.eks.main.tf variables.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -15,9 +15,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
+      desired_size   = 2
       min_size       = 2
       max_size       = 5
-      desired_size   = 2
       instance_types = ["t3.large"]
     }
   }
---
+++ modules/eks/variables.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs for EKS"
+  type        = list(string)
+}
